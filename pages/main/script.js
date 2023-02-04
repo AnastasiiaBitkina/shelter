05943679e4d4f96b30170820fabeebc7f5fedc0b
anastasia <anastasia.ethno@gmail.com>
2023-02-04 22:38:19 +0100

    feat: add id

---
+++ pages/main/script.js
@@ -0,0 +1,32 @@
+/*slider*/
+
+let petSlider = document.getElementById("viewport").offsetWidth;
+let btnNext = document.getElementById("next");
+let btnPrev = document.getElementById("prev");
+let slider = document.querySelector("div.slider");
+let viewSliders = document.querySelectorAll(".viewSlide");
+let viewSlide = 0;
+
+viewSliders[0].style.backgroundColor = "green";
+
+btnNext.addEventListener("click", function () {
+    viewSliders[viewSlide].style.backgroundColor = "red";
+    if (viewSlide < 2) {
+        viewSlide++
+    } else {
+        viewSlide = 0;
+    }
+    viewSliders[viewSlide].style.backgroundColor = "green";
+    slider.style.left = -viewSlide * viewport + "px";
+});
+
+btnPrev.addEventListener("click", function () {
+    viewSliders[viewSlide].style.backgroundColor = "red";
+    if (viewSlide > 0) {
+        viewSlide--;
+    } else {
+        viewSlide = 2;
+    }
+    viewSliders[viewSlide].style.backgroundColor = "green";
+    slider.style.left = -viewSlide * viewport + "px";
+})
