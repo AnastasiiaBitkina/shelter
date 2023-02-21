efff06c3f031eed15f843746bd3c675b4ccbf625
anastasia <anastasia.ethno@gmail.com>
2023-02-21 19:42:23 +0100

    feat: add popups

--- pages/main/script.js
+++ pages/main/script.js
@@ -17,3 +17,41 @@ document.addEventListener('click', (e) =>{
         popup.classList.remove('active');
     }
 });
+/*popup_2*/
+let popupBg2 = document.querySelector('.popup__bg_2');
+let popup2 = document.querySelector('.popup_2');
+let openPopupButton2 = document.querySelectorAll('.open-popup_2');
+
+openPopupButton2.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg2.classList.add('active');
+        popup2.classList.add('active');
+    })
+});
+
+document.addEventListener('click', (e) =>{
+    if(e.target === popupBg2) {
+        popupBg2.classList.remove('active');
+        popup2.classList.remove('active');
+    }
+});
+/*popup_3*/
+let popupBg3 = document.querySelector('.popup__bg_3');
+let popup3 = document.querySelector('.popup_3');
+let openPopupButton3 = document.querySelectorAll('.open-popup_3');
+
+openPopupButton3.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg3.classList.add('active');
+        popup3.classList.add('active');
+    })
+});
+
+document.addEventListener('click', (e) =>{
+    if(e.target === popupBg3) {
+        popupBg3.classList.remove('active');
+        popup3.classList.remove('active');
+    }
+});
